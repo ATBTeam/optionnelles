4fada7316bcf4b8f5e9e982f2ed7ec82c097579b
HoangCao <truonghcit@gmail.com>
2015-07-01 18:11:43 +0200

    Seed de test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Specialite;
+use App\Parcours;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +16,36 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        // clear our database ------------------------------------------
+        DB::table('specialite')->delete();
+        DB::table('parcours')->delete();
+        DB::table('groupe')->delete();
+        DB::table('user')->delete();
+        DB::table('profil')->delete();
+        DB::table('choix')->delete();
+        DB::table('enseigner')->delete();
+        DB::table('ue')->delete();
+        DB::table('parcours_ue')->delete();
+
+        $specialite = Specialite::create(array(
+            'intitule' => 'MIAGE',
+            'description' => 'Méthodes Informatiques Appliquées à la Gestion de l\'Entreprise'
+        ));
+
+        $parcours = Parcours::create(array(
+            'intitule' => 'M1 MIAGE',
+            'description' => 'Master 1 Miage',
+            'annee' => 1,
+            'nb_opt_s1' => 10,
+            'nb_opt_s2' => 10,
+            'deb_choix_s1' => '0000-00-00 00:00:00',
+            'fin_choix_s1' => '0000-00-00 00:00:00',
+            'deb_choix_s2' => '0000-00-00 00:00:00',
+            'fin_choix_s2' => '0000-00-00 00:00:00',
+            'id_specialite' => $specialite->id
+        ));
+
+        $this->command->info('Parfait, vérifier ta base de données');
 
         Model::reguard();
     }
