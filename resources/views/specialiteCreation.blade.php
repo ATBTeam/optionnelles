83c01990f09ee801ae5e28b716b636abab5402f1
DeathKnightDK <thomas.leclerc1@wanadoo.fr>
2015-07-04 20:49:08 +0200

    thomas routes

--- resources/views/specialiteCreation.blade.php
+++ resources/views/specialiteCreation.blade.php
@@ -1,9 +1,23 @@
 @extends('template')
 
 @section('contenu')
-    {!! Form::open(['url' => 'specialite/creation']) !!}
-    {!! Form::label('nom', 'Entrez le nom : ') !!}
-    {!! Form::text('nom') !!}
-    {!! Form::submit('Envoyer !') !!}
-    {!! Form::close() !!}
+    <br>
+    <div class="col-sm-offset-3 col-sm-6">
+        <div class="panel panel-info">
+            <div class="panel-heading">Créer Spécialité</div>
+            <div class="panel-body">
+                {!! Form::open(['url' => 'specialite/creation']) !!}
+                <div class="form-group {!! $errors->has('nom') ? 'has-error' : '' !!}">
+                    {!! Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'nom de la spécialité']) !!}
+                    {!! $errors->first('nom', '<small class="help-block">:message</small>') !!}
+                </div>
+                <div class="form-group {!! $errors->has('texte') ? 'has-error' : '' !!}">
+                    {!! Form::textarea ('texte', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
+                    {!! $errors->first('texte', '<small class="help-block">:message</small>') !!}
+                </div>
+                {!! Form::submit('Envoyer !', ['class' => 'btn btn-info pull-right']) !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
 @stop
