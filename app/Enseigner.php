6227b0f33c87b1ac52c42aa4d021eff3b693da11
HoangCao <truonghcit@gmail.com>
2015-07-06 05:48:22 +0200

    mise à jour de truong

---
+++ app/Enseigner.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Enseigner extends Model
+{
+    protected $table = 'enseigner';
+    protected $fillable = ['ue_id', 'user_id'];
+    public function ue()
+    {
+        return $this->belongsTo('App\Ue'); // this matches the Eloquent model
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User'); // this matches the Eloquent model
+    }
+}
