3bbe79c28aa00e1955c316034615c56967693b66
DeathKnightDK <thomas.leclerc1@wanadoo.fr>
2015-07-07 07:42:27 +0200

    thomas

--- resources/views/accueil/accueilAdmin.blade.php
+++ resources/views/accueil/accueilAdmin.blade.php
@@ -2,52 +2,5 @@
 
 @section('contenu')
 <h1>Bienvenue sur la gestion des optionnelles ! (Admin)</h1>
-<div class="row">
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <a href="/optionnelles/public/doc/pdf_doc_example.pdf" TARGET="_blank">
-                <img src="/optionnelles/public/img/adobe-pdf-logo.jpg" width="100">
-            </a>
-            <div class="caption">
-                <h3>MIAGE1</h3>
-                <p>Programme de la MIAGE 1 et description des UEs</p>
-                <p><a href="/optionnelles/public/doc/pdf_doc_example.pdf" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <a href="/optionnelles/public/doc/pdf_doc_example.pdf" TARGET="_blank">
-                <img src="/optionnelles/public/img/adobe-pdf-logo.jpg" width="100">
-            </a>
-            <div class="caption">
-                <h3>MIAGE2</h3>
-                <p>Programme de la MIAGE 2 et description des UEs</p>
-                <p><a href="/optionnelles/public/doc/pdf_doc_example.pdf" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-        </div>
-    </div>
-</div>
+
 @stop
--- resources/views/accueil/accueilEtud.blade.php
+++ resources/views/accueil/accueilEtud.blade.php
@@ -2,52 +2,89 @@
 
 @section('contenu')
 <h1>Bienvenue sur la gestion des optionnelles !</h1>
-<div class="row">
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <a href="/optionnelles/public/doc/pdf_doc_example.pdf" TARGET="_blank">
-                <img src="/optionnelles/public/img/adobe-pdf-logo.jpg" width="100">
-            </a>
-            <div class="caption">
-                <h3>MIAGE1</h3>
-                <p>Programme de la MIAGE 1 et description des UEs</p>
-                <p><a href="/optionnelles/public/doc/pdf_doc_example.pdf" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
-                </p>
-            </div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc1.pdf')}}" TARGET="_blank">
+            <img src="{{url('img/adobe-pdf-logo.jpg')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc1</h3>
+            <p>Commentaire Doc 1</p>
+            <p>
+                <a href="{{url('doc/doc1.pdf')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
         </div>
     </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <a href="/optionnelles/public/doc/pdf_doc_example.pdf" TARGET="_blank">
-                <img src="/optionnelles/public/img/adobe-pdf-logo.jpg" width="100">
-            </a>
-            <div class="caption">
-                <h3>MIAGE2</h3>
-                <p>Programme de la MIAGE 2 et description des UEs</p>
-                <p><a href="/optionnelles/public/doc/pdf_doc_example.pdf" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
-                </p>
-            </div>
+</div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc2.pdf')}}" TARGET="_blank">
+            <img src="{{url('img/adobe-pdf-logo.jpg')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc2</h3>
+            <p>Commentaire Doc 1</p>
+            <p>
+                <a href="{{url('doc/doc2.pdf')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
         </div>
     </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
+</div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc3.pdf')}}" TARGET="_blank">
+            <img src="{{url('img/adobe-pdf-logo.jpg')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc3</h3>
+            <p>Commentaire Doc 1</p>
+            <p>
+                <a href="{{url('doc/doc3.pdf')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
         </div>
     </div>
-    <div class="col-sm-5 col-md-3">
-        <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
+</div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc4.pdf')}}" TARGET="_blank">
+            <img src="{{url('img/adobe-pdf-logo.jpg')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc4</h3>
+            <p>Commentaire Doc 1</p>
+            <p>
+                <a href="{{url('doc/doc4.pdf')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
         </div>
     </div>
+</div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc5.pdf')}}" TARGET="_blank">
+            <img src="{{url('img/adobe-pdf-logo.jpg')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc5</h3>
+            <p>Commentaire Doc 1</p>
+            <p>
+                <a href="{{url('doc/doc5.pdf')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-5 col-md-3">
+    <div class="thumbnail">
+        <a href="{{url('doc/doc6.csv')}}" TARGET="_blank">
+            <img src="{{url('img/csv.png')}}" width="100">
+        </a>
+        <div class="caption">
+            <h3>doc6</h3>
+            <p>Commentaire Doc 6</p>
+            <p>
+                <a href="{{url('doc/doc6.csv')}}" class="btn btn-primary" TARGET="_blank" role="button">Ouvrir</a>
+            </p>
+        </div>
+    </div>
+</div>
 </div>
 @stop
